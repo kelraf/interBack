f54cc64b0d981f1e86dfee0bd5be831878568d5d
kelraf <kelraf11746@gmail.com>
2021-09-15 20:58:25 +0300

    heroku

--- .history/config/prod_20210915205057.exs
+++ .history/config/prod_20210915205057.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :interBack, InterBackWeb.Endpoint,
-  url: [host: "https://inter-back.herokuapp.com/", port: 443],
+  url: [host: "https://inter-back.herokuapp.com/", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
